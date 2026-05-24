e7fcd656f996648ec044f857d5169588e40a0242
129cathy129-droid <129cathy129@gmail.com>
2026-05-24 22:08:58 +0900

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -235,6 +235,10 @@ function start() {
 
   /** Disable all checkboxes and hide/show appropriate parts while we preload the images. */
   document.querySelectorAll('input[type=checkbox]').forEach(cb => cb.disabled = true);
+  document.querySelectorAll('.sorting.button').forEach(el => el.style.display = 'block');
+  document.querySelectorAll('.sort.text').forEach(el => el.style.display = 'block');
+  document.querySelector('.progress').style.display = 'block';
+	
   loading = false;
   display();
 }
